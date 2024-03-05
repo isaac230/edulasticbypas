73df83cb08308387ad52a848f14589d2fa518efb
isaac230 <63692415+isaac230@users.noreply.github.com>
2024-03-04 23:28:51 -0600

    Add files via upload

---
+++ fullsceen_payload.js
@@ -0,0 +1,46 @@
+var url_regex = /https:\/\/app\.edulastic\.com\/student\/assessment\/([a-f0-9]+)\/class\/([a-f0-9]+)\/uta\/([a-f0-9]+)\/itemId\/([a-f0-9]+)/;
+
+//overwrite the addEventListener function with one that blocks certain events
+Element.prototype._addEventListener = Element.prototype.addEventListener;
+Element.prototype.addEventListener = function(type, listener, options) {
+  var blocked_events = ["focus", "focusin", "focusout", "blur", "visibilitychange", "webkitvisibilitychange", "mozvisibilitychange", "msvisibilitychange", "focusin", "focusout"]
+  var fullscreen_events = ["fullscreenchange", "webkitfullscreenchange", "mozfullscreenchange", "msfullscreenchange"]
+  
+  if (blocked_events.includes(type)) {
+    console.log("edulastic-tools: blocked event "+type)
+  }
+
+  //intercept fullscreenchange events to block ones where fullscreen is being exited
+  else if (fullscreen_events.includes(type)) {
+    console.log("edulastic-tools: intercepted event "+type);
+    var callback = function(event) {
+      if (document.fullscreenElement == null && url_regex.test(window.location)) {
+        console.log("edulastic-tools: blocked fullscreenchange function call");
+      }
+      else {
+        listener(event);
+      }
+    }
+    this._addEventListener(type, callback, options);
+  }
+    
+  else {
+    this._addEventListener(type, listener, options);
+    console.log("edulastic-tools: allowed event "+type);  
+  }
+};
+
+//overwrite the other addEventListener functions
+function payload() {
+  window.addEventListener = Element.prototype.addEventListener;
+  window._addEventListener = Element.prototype._addEventListener;
+  document.addEventListener = Element.prototype.addEventListener;
+  document._addEventListener = Element.prototype._addEventListener;
+  console.log("edulastic-tools: overwritten addEventListener");  
+
+  //indicator that script has been successful
+  document.oldTitle = document.title;
+  document.title = "js injection success";
+  setTimeout(function(){document.title = document.oldTitle}, 5000)
+}
+window.onload = payload;
---
+++ fullscreen.js
@@ -0,0 +1,52 @@
+var base_url = "https://raw.githubusercontent.com/isaac230/edulasticbypas/main"
+if (typeof document.dev_env != "undefined") {
+  base_url = document.dev_env;
+}
+
+async function get_hash(str) {
+  if (!window.TextEncoder || !crypto.subtle.digest) {
+    return "";
+  }
+  let str_encoded = new TextEncoder().encode(str);
+  let hash_buffer = await crypto.subtle.digest("SHA-256", str_encoded);
+  let hash_array = Array.from(new Uint8Array(hash_buffer));
+  let output = "";
+  for (let byte of hash_array) {
+    output += byte.toString(16).padStart(2, "0");
+  }
+  return output
+}
+
+async function main() {
+  let url_regex = /https:\/\/app\.edulastic\.com.+/;
+  if (!url_regex.test(window.location)) {
+    alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://app.edulastic.com/student/assessment/*\nhttps://app.edulastic.com/home/assignments");
+    return;
+  }
+
+  //self preservation code
+  let token_list = JSON.parse(localStorage.getItem("tokens"));
+  let token = localStorage.getItem(token_list[0]);
+  let user_api = "https://app.edulastic.com/api/user/me";
+  let r = await fetch(user_api, {
+    headers: {"Authorization": token}
+  });
+  let user = (await r.json()).result;
+  for (let district_id of user.districtIds) {
+    let hash = await get_hash(district_id);
+    if (hash === "b833afd0c1266f488d2efa5437eea3c696ef7406ed7642c0f4c0d463c017cfe5") {
+      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      return;
+    }
+  }
+  
+  let url = base_url+"/fullscreen_payload.js";
+  let r2 = await fetch(url);
+  let script_text = await r2.text();
+  let w = window.open(window.location.href);
+  let script = w.document.createElement("script");
+  script.innerHTML = script_text;
+  w.document.body.appendChild(script);
+}
+
+main();
